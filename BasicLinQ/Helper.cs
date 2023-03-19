0a927da1bf16d0e6736d5d17168afd1dd0069d25
TrucNguyen <truc.nguyen@saigontechnology.vn>
2023-03-19 23:54:12 +0700

    add some setting to show result

---
+++ BasicLinQ/Helper.cs
@@ -0,0 +1,13 @@
+﻿using BasicLinQ.Entities;
+using ConsoleTables;
+
+namespace BasicLinQ
+{
+    public static class Helper
+    {
+        public static void LogListData<T>(IEnumerable<T> list)
+        {
+            ConsoleTable.From(list).Write();
+        }
+    }
+}
